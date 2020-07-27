713739bed7d4bc4f435c52d1dddcd55fe299861c
John Distasio <jndistasio@gmail.com>
2020-07-27 19:17:53 -0400

    Add publish-to-redis experiment

--- main.go
+++ main.go
@@ -2,12 +2,74 @@ package main
 
 import (
 	"context"
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 )
 
+const room = "test"
+
 var upgrader = websocket.Upgrader{}
+
+type SignalRelay struct {
+	ctx context.Context
+	ws  *WebsocketProxy
+	rdb *redis.Client
+}
+
+func StartSignalRelay(ctx context.Context, proxy *WebsocketProxy) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pipe := rdb.TxPipeline()
+	incr := pipe.Incr(ctx, room)
+	pipe.Expire(ctx, room, time.Minute)
+
+	_, err := pipe.Exec(ctx)
+	log.Println(incr.Val(), err)
+
+	relay := &SignalRelay{
+		ctx: context.Background(),
+		ws:  proxy,
+		rdb: rdb,
+	}
+
+	relay.ws.Start()
+	go relay.from()
+}
+
+func (s *SignalRelay) from() {
+	for {
+		select {
+		case input := <-s.ws.Recv():
+			log.Printf("received %s\n", input)
+
+			err := s.rdb.Publish(s.ctx, "room:test", input).Err()
+			if err != nil {
+				panic(err)
+			}
+			/*
+				// example of dealing with data received from websocket
+				switch string(input) {
+				case "goodbye":
+					log.Print("hanging up!")
+					return
+				default:
+				}
+			*/
+
+		default:
+		}
+	}
+}
+
+func (s *SignalRelay) to() {
+}
 
 func ws(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -20,7 +82,8 @@ func ws(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("new websocket connection from %s\n", conn.RemoteAddr().String())
 
-	go NewTimeTicker(NewWebsocketProxy(context.Background(), conn)).start()
+	StartSignalRelay(r.Context(),
+		NewWebsocketProxy(context.Background(), conn))
 }
 
 func main() {
