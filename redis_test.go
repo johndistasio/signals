798adbcbb15278fe1969401d97c50815c81abb58
John Distasio <jndistasio@gmail.com>
2020-08-09 13:40:05 -0400

    Replace handroll mock redis with testify mock redis

--- redis_test.go
+++ redis_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -42,14 +43,6 @@ func (m *MockRedis) ScriptLoad(ctx context.Context, script string) *redis.String
 	return args.Get(0).(*redis.StringCmd)
 }
 
-type mockRedis struct {}
-
-func (r *mockRedis) Subscribe(context.Context, ...string) *redis.PubSub {return nil}
-func (r *mockRedis) Publish(context.Context, string, interface{}) *redis.IntCmd {return redis.NewIntResult(0, nil)}
-func (r *mockRedis) Eval(context.Context, string, []string, ...interface{}) *redis.Cmd {return nil}
-func (r *mockRedis) EvalSha(context.Context, string, []string, ...interface{}) *redis.Cmd {return nil}
-func (r *mockRedis) ScriptExists(context.Context, ...string) *redis.BoolSliceCmd {return nil}
-func (r *mockRedis) ScriptLoad(context.Context, string) *redis.StringCmd {return nil}
 
 type slowRedis struct {}
 
@@ -66,29 +59,6 @@ func (r *slowRedis) EvalSha(context.Context, string, []string, ...interface{}) *
 func (r *slowRedis) ScriptExists(context.Context, ...string) *redis.BoolSliceCmd {return nil}
 func (r *slowRedis) ScriptLoad(context.Context, string) *redis.StringCmd {return nil}
 
-func TestRedisRoomPublish(t *testing.T) {
-	room := &RedisRoom{name: "test", joined: true, rdb: &mockRedis{}}
-
-	err := room.Publish(context.Background(), []byte{})
-
-	if err != nil {
-		t.Errorf("%v\n", err)
-	}
-}
-
-func TestRedisRoomPublishCancellation(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	room := &RedisRoom{name: "test", joined: true, rdb: &slowRedis{}}
-
-	err := room.Publish(ctx, []byte{})
-
-	if err != context.Canceled {
-		t.Errorf("%v\n", err)
-	}
-}
-
 func TestRedisRoomReceive(t *testing.T) {
 	ch := make(chan *redis.Message, 1)
 	defer close(ch)
@@ -97,7 +67,7 @@ func TestRedisRoomReceive(t *testing.T) {
 
 	ch <- &redis.Message{Channel: "test", Pattern: "test", Payload: expected}
 
-	room := &RedisRoom{name: "test", joined: true, ch: ch, rdb: &mockRedis{}}
+	room := &RedisRoom{name: "test", joined: true, ch: ch, rdb: new(MockRedis)}
 
 	message, err := room.Receive(context.Background())
 
@@ -119,6 +89,49 @@ func TestRedisRoomReceiveCancellation(t *testing.T) {
 	room := &RedisRoom{name: "test", joined: true, rdb: &slowRedis{}}
 
 	_, err := room.Receive(ctx)
+
+	if err != context.Canceled {
+		t.Errorf("%v\n", err)
+	}
+}
+
+func TestRedisRoomPublish(t *testing.T) {
+	cmd := redis.NewIntResult(1, nil)
+
+	rdb := new(MockRedis)
+	rdb.On("Publish", mock.Anything, mock.Anything, mock.Anything).Return(cmd)
+
+	room := &RedisRoom{name: "test", joined: true, rdb: rdb}
+
+	err := room.Publish(context.Background(), []byte{})
+
+	if err != nil {
+		t.Errorf("expected %v, got %v\n", nil, err)
+	}
+}
+
+func TestRedisRoomPublishError(t *testing.T) {
+	cmd := redis.NewIntResult(0, errors.New("publish"))
+
+	rdb := new(MockRedis)
+	rdb.On("Publish", mock.Anything, mock.Anything, mock.Anything).Return(cmd)
+
+	room := &RedisRoom{name: "test", joined: true, rdb: rdb}
+
+	err := room.Publish(context.Background(), []byte{})
+
+	if err != ErrRoomGone {
+		t.Errorf("expected %v, got %v\n", ErrRoomGone, err)
+	}
+}
+
+func TestRedisRoomPublishCancellation(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	room := &RedisRoom{name: "test", joined: true, rdb: &slowRedis{}}
+
+	err := room.Publish(ctx, []byte{})
 
 	if err != context.Canceled {
 		t.Errorf("%v\n", err)
