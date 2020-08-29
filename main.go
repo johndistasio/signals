1874dc0bd1c85e573ef11c7a4b66ce736951ea0a
John Distasio <jndistasio@gmail.com>
2020-08-29 14:01:38 -0400

    Move HeaderPrinter to debug

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jaegerConfig "github.com/uber/jaeger-client-go/config"
@@ -10,42 +9,7 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"sort"
 )
-
-type HeaderPrinter struct {}
-
-func (c *HeaderPrinter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	header := `
-<!DOCTYPE html>
-<html>
-<body>
-`
-	footer := `
-</body>
-</html>
-`
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte(header))
-
-	headers := make([]string, len(r.Header))
-
-	for k, _ := range r.Header {
-		headers = append(headers, k)
-	}
-
-	sort.Strings(headers)
-
-	for _, name := range headers {
-		for _, value := range r.Header[name] {
-			_, _ = w.Write([]byte(fmt.Sprintf("<p><b>%s:</b> %v</p>", name, value)))
-		}
-	}
-
-	_, _ = w.Write([]byte(footer))
-}
-
 
 func main() {
 	// Sample configuration for testing. Use constant sampling to sample every trace
