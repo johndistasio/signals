ef192d67ca9ba4a664b673474e5723a960b2bdef
John Distasio <1438521+johndistasio@users.noreply.github.com>
2020-12-13 15:24:19 -0500

    Use JSON messages instead of headers to pass client state (#8)
    * Encapsulate ws state in struct
    
    * Log received websocket messages
    
    * Initial ws handshake impl
    
    * wip ws handshake cleanup
    
    * Don't use HTTP headers for websocket authentication
    
    * Add test page
    
    * Remove header from seat handler
    
    * Remove header from signal handler
    
    * standardize middleware interface
    
    * Do json encoding inside event publisher
    
    * reorganize main
    
    * wip readme updates
    
    * Stop returning backend error details to clients
    
    * Extend test page for back-and-forth messaging
    
    * Set a reasonable default status
    
    * Log error to renew lease on ping appropriately
    
    * Start test page webserver on port 8181
    
    * Handle connection errors on test page
    
    * Update README
    
    * Update CI configuration

--- mocks/publisher.go
+++ mocks/publisher.go
@@ -9,7 +9,7 @@ type Publisher struct {
 	mock.Mock
 }
 
-func (m *Publisher) Publish(ctx context.Context, channel string, payload []byte) error {
-	args := m.Called(ctx, channel, payload)
+func (m *Publisher) Publish(ctx context.Context, channel string, event interface{}) error {
+	args := m.Called(ctx, channel, event)
 	return args.Error(0)
 }
